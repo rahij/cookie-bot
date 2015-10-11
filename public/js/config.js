731302de8d2473dd14b0c4d2ecf01d9179f476f6
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2015-10-12 03:06:27 +0800

    initial work to save files

--- public/js/config.js
+++ public/js/config.js
@@ -2,11 +2,14 @@
 
 svgEditor.setConfig({
 extensions: [
+  // 'ext-server_opensave.js',
 	// 'ext-eyedropper.js',
 	// 'ext-shapes.js',
 	// 'ext-polygon.js',
 	// 'ext-star.js'
 ],
 //emptyStorageOnDecline: true
-	allowedOrigins: [window.location.origin] // May be 'null' (as a string) when used as a file:// URL
+	allowedOrigins: [window.location.origin], // May be 'null' (as a string) when used as a file:// URL
+  showRulers: false,
+  extPath: 'js/extensions/',
 });
