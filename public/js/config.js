63d17be7831ee790dcf8907b0922939bbaa3ed1a
Rahij Ramsharan <rahij.ramsharan@gmail.com>
2015-10-20 18:39:43 +0800

    hide unnecessary items in svg edit

--- public/js/config.js
+++ public/js/config.js
@@ -10,6 +10,9 @@ extensions: [
 //emptyStorageOnDecline: true
 	allowedOrigins: [window.location.origin], // May be 'null' (as a string) when used as a file:// URL
   showRulers: false,
+  no_save_warning: true,
+  noDefaultExtensions: true,
+  show_outside_canvas: false,
   extPath: 'js/extensions/',
   langPath: 'js/locale/',
 });
